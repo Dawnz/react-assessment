9675aa0a1d0f30a665dff481674233a258a0b052
Dawnz <dawnz1@hotmail.com>
2021-06-28 21:14:42 -0500

    created axios to fetch data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,17 @@
+import axios from "axios";
+
 function App() {
+  const url = 'https://swapi.dev/api/';
+  const getAllPeople = () => {
+    axios.get(`${url}people/1`)
+      .then((response) => {
+        const allPeople = response.data
+        console.log(allPeople);
+      })
+      .catch(error => console.error(`Error: ${error}`))
+  }
+  getAllPeople();
+
   return (
     <div className="App">
       <p>hello world</p>
